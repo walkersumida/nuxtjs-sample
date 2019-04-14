c8eea6d3c95e3d4402a4dcc065cf65755234df27
WalkerSumida <walkersumida@gmail.com>
2019-04-14 16:48:27 +0900

    fix: error(Please use export @dec class instead)

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,10 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+    ecmaFeatures: {
+      legacyDecorators: true
+    }
   },
   extends: [
     '@nuxtjs',
