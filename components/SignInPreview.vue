a368e285a5e757b2e09d4717bcf9989d3f8719fc
WalkerSumida <walkersumida@gmail.com>
2019-04-20 16:22:00 +0900

    feat: add sign in page/component

---
+++ components/SignInPreview.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>
+      <input v-model="email" type="text" placeholder="email" />
+    </p>
+    <p>
+      <input v-model="password" type="password" placeholder="password" />
+    </p>
+    <p>
+      <input value="Sign In" type="submit" @click="onClick" />
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import axios from '~/plugins/axios'
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class SignInPreview extends Vue {
+  async onClick() {
+    const params = new URLSearchParams()
+    params.append('email', this.email)
+    params.append('password', this.password)
+    try {
+      const res = await axios.post('/auth/sign_in', params)
+      console.log('success')
+      console.log(res)
+    } catch (error) {
+      console.log('error')
+      console.log(error)
+    }
+  }
+}
+</script>
---
+++ pages/sign_in.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <SignInPreview />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  components: {
+    SignInPreview: () => import('~/components/SignInPreview.vue')
+  }
+})
+export default class SignIn extends Vue {}
+</script>
