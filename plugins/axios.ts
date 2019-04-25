bfa53e3bd7fc77f1fef025afca86adbf35eff086
WalkerSumida <walkersumida@gmail.com>
2019-04-25 23:27:16 +0900

    feat: seva header values

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,5 +1,69 @@
 import axios from 'axios'
+import * as Cookies from 'js-cookie'
 
-export default axios.create({
+interface Headers {
+  'access-token': string
+  client: string
+  uid: string
+  Accept?: string
+  'cache-control'?: string
+  'content-type'?: string
+  expiry?: string
+  'token-type'?: string
+}
+
+const removeCookies = () => {
+  Cookies.remove('headerAccessToken')
+  Cookies.remove('headerClient')
+  Cookies.remove('headerUid')
+  Cookies.remove('headerExpiry')
+}
+
+const setHeaders = (headers: Headers) => {
+  headers['access-token'] = Cookies.get('headerAccessToken')
+  headers['client'] = Cookies.get('headerClient')
+  headers['uid'] = Cookies.get('headerUid')
+  return headers
+}
+
+const setCookie = (name: string, value: string) => {
+  // TODO: `secure: true` when not dev
+  Cookies.set(name, value, { secure: false })
+}
+
+const setCookies = (headers: Headers) => {
+  setCookie('headerClient', headers['client'])
+  setCookie('headerUid', headers['uid'])
+  if(headers['access-token']) {
+    setCookie('headerAccessToken', headers['access-token'])
+  }
+  if(headers['expiry']) {
+    setCookie('headerExpiry', headers['expiry'])
+  }
+}
+
+const axiosInstance = axios.create({
   baseURL: process.env.apiUrl
 })
+
+axiosInstance.interceptors.request.use((config) => {
+  console.log('inter req:', config)
+  config.headers = setHeaders(config.headers)
+  return config
+}, (error) => {
+  console.log('inter req err:', error)
+  return Promise.reject(error)
+})
+
+axiosInstance.interceptors.response.use((response) => {
+  // See https://devise-token-auth.gitbook.io/devise-token-auth/conceptual
+  console.log('inter res:', response)
+  setCookies(response.headers)
+  return response
+}, (error) => {
+  // TODO: redirect sign in page if 401 error
+  console.log('inter res err:', error)
+  removeCookies()
+  return Promise.reject(error)
+})
+export default axiosInstance
