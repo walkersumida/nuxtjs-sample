70436948444c1daea783ef8e0a0cd25aa379e892
WalkerSumida <walkersumida@gmail.com>
2019-07-02 00:27:43 +0900

    fix: show bar when not login

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,7 @@
       fixed
       app
     >
-      <v-list>
+      <v-list v-if="!isSignInPage()">
         <v-list-tile
           v-for="(item, i) in items"
           :key="i"
@@ -25,7 +25,7 @@
       </v-list>
     </v-navigation-drawer>
     <v-toolbar :clipped-left="clipped" fixed app>
-      <v-toolbar-side-icon @click="drawer = !drawer" />
+      <v-toolbar-side-icon v-if="!isSignInPage()" @click="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
     </v-toolbar>
     <v-content>
@@ -62,7 +62,10 @@ export default class LayoutDefault extends Vue {
         }
       ],
       miniVariant: false,
-      title: 'Nuxtjs x Rails'
+      title: 'Nuxtjs x Rails',
+      isSignInPage: () => {
+        return this.$route.path === '/sign_in'
+      }
     }
   }
 }
