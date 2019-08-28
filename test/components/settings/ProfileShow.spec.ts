21083a346168bdbfec8fff3dc1277d31ccf39bb4
WalkerSumida <walkersumida@gmail.com>
2019-08-28 23:11:00 +0900

    test: add ProfileShow component

---
+++ test/components/settings/ProfileShow.spec.ts
@@ -0,0 +1,26 @@
+import { shallowMount } from "@vue/test-utils"
+import ProfileShow from "@/components/settings/ProfileShow.vue"
+
+describe("ProfileShow component", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(ProfileShow, {
+      propsData: {
+        user: { nickname: 'demo' }
+      }
+    });
+  });
+
+  it("has the expected html structure", () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it("has the expected nickname element", () => {
+    expect(wrapper.contains('v-text-field[label="Nickname"]')).toBe(true)
+  })
+
+  it("has the expected button element", () => {
+    expect(wrapper.contains('v-btn[disabled="true"]')).toBe(true)
+  })
+});
