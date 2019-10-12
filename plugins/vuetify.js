ef2a42fe61cde33a838e8acf340847c85f03c02e
WalkerSumida <walkersumida@gmail.com>
2019-10-12 17:39:35 +0900

    refact: base colors

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -2,30 +2,30 @@ import colors from 'vuetify/es5/util/colors'
 
 var options = {
   theme: {
-    dark: false,
+    dark: true,
     themes: {
       light: {
         primary: colors.blue.darken2,
         accent: colors.grey.darken3,
         secondary: colors.amber.darken3,
-        info: colors.teal.lighten1,
+        info: colors.lightBlue.lighten1,
         warning: colors.amber.base,
         error: colors.deepOrange.accent4,
         success: colors.green.accent3
       },
       dark: {
-        primary: colors.blue.darken2,
-        accent: colors.grey.darken3,
-        secondary: colors.amber.darken3,
-        info: colors.teal.lighten1,
-        warning: colors.amber.base,
-        error: colors.deepOrange.accent4,
-        success: colors.green.accent3
+        primary: colors.blue.darken4,
+        accent: colors.grey.darken4,
+        secondary: colors.amber.darken4,
+        info: colors.lightBlue.darken3,
+        warning: colors.amber.darken4,
+        error: colors.deepOrange.darken4,
+        success: colors.green.darken4
       }
     }
   },
   icons: {
-    iconfont: 'fa',
+    iconfont: 'fa4',
     values: {
       post: 'fa fa-lg fa-file',
       profile: 'fa fa-user fa-lg',
